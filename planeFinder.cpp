11ceaa485c2ac19ce0f7eff1eea6824471870405
crawfordhmc <43801183+crawfordhmc@users.noreply.github.com>
2020-10-26 19:12:44 +1300

    Update planeFinder.cpp
    Manually updating master due to merge issues with IDE files - view lab branch for development history

--- planeFinder.cpp
+++ planeFinder.cpp
@@ -1,82 +1,62 @@
-#include "PointCloud.h"
+#include "UniformPC.h"
+#include "OctreePC.h"
 
 void initialiseColours(std::vector<Eigen::Vector3i>* colours) {
     colours->resize(11);
 
-    (*colours)[0](0) = 255;    (*colours)[0](1) = 0;      (*colours)[0](2) = 0;
-    (*colours)[1](0) = 0;      (*colours)[1](1) = 255;    (*colours)[1](2) = 0;
-    (*colours)[2](0) = 0;      (*colours)[2](1) = 0;      (*colours)[2](2) = 255;
-    (*colours)[3](0) = 255;    (*colours)[3](1) = 255;    (*colours)[3](2) = 0;
-    (*colours)[4](0) = 0;      (*colours)[4](1) = 255;     (*colours)[4](2) = 255;
-    (*colours)[5](0) = 255;    (*colours)[5](1) = 0;      (*colours)[5](2) = 255;
-    (*colours)[6](0) = 0;      (*colours)[6](1) = 0;      (*colours)[6](2) = 0;
-    (*colours)[7](0) = 255;    (*colours)[7](1) = 255;    (*colours)[7](2) = 255;
-    (*colours)[8](0) = 127;    (*colours)[8](1) = 0;      (*colours)[8](2) = 0;
-    (*colours)[9](0) = 0;      (*colours)[9](1) = 127;    (*colours)[9](2) = 0;
-    (*colours)[10](0) = 0;     (*colours)[10](1) = 0;     (*colours)[10](2) = 127;
+    (*colours)[0](0) = 255;    (*colours)[0](1) = 0;      (*colours)[0](2) = 0; //red
+    (*colours)[1](0) = 0;      (*colours)[1](1) = 255;    (*colours)[1](2) = 0; //green
+    (*colours)[2](0) = 0;      (*colours)[2](1) = 0;      (*colours)[2](2) = 255; //blue
+    (*colours)[3](0) = 255;    (*colours)[3](1) = 255;    (*colours)[3](2) = 0; //yellow
+    (*colours)[4](0) = 0;      (*colours)[4](1) = 255;     (*colours)[4](2) = 255; //cyan
+    (*colours)[5](0) = 255;    (*colours)[5](1) = 0;      (*colours)[5](2) = 255; //magenta
+    (*colours)[6](0) = 0;      (*colours)[6](1) = 0;      (*colours)[6](2) = 0; //black
+    (*colours)[7](0) = 255;    (*colours)[7](1) = 255;    (*colours)[7](2) = 255; //white
+    (*colours)[8](0) = 127;    (*colours)[8](1) = 0;      (*colours)[8](2) = 0; //dark red
+    (*colours)[9](0) = 0;      (*colours)[9](1) = 127;    (*colours)[9](2) = 0; //dark green
+    (*colours)[10](0) = 0;     (*colours)[10](1) = 0;     (*colours)[10](2) = 127; //dark blue
 }
 
 
-//idea: get user estimate of minimum number of planes
-// When running loads of trials, keep that many of the next largest planes
-// When a plane is found, run through the next largest planes and reassign points by distance, then re-rank and save
-// Then run more trials if % of the scene is not explained
 
-PointCloud ransac(PointCloud pointCloud, std::mt19937 gen, double successProb, double explained, double threshold, unsigned int maxTrials) {
+std::vector<Eigen::Hyperplane<double, 3>> ransac(PointCloud& pointCloud, std::mt19937 gen, double successProb, double noise, double threshold, unsigned int maxTrials) {
 
     unsigned int plane = 0;
-    std::vector<size_t> removedPoints;
-    int threads;
-    omp_set_num_threads(4);
+    std::vector<Eigen::Hyperplane<double, 3>> planes;
 
     do {
         // Initial number of trials, very high from lowball initial inlier ratio
-        double inlierRatio = 0.1;
+        double inlierRatio = 0.01;
         unsigned int numTrials = log(1 - successProb) / log(1 - pow(inlierRatio, 3));
         // Create random distribution for the point cloud, with other planes removed
-        std::uniform_int_distribution<size_t> distr(0, pointCloud.size() - 1);
+        std::uniform_int_distribution<size_t> distr(0, pointCloud.remainingPoints.size() - 1);
         Eigen::Hyperplane<double, 3> bestPlane;
-        std::vector<size_t> bestPoints;
+        std::vector<bool> bestPoints;
+        size_t bestSize = 0;
 
-        int trial = 0;
+        unsigned int trial = 0;
         while (trial < numTrials && trial < maxTrials) {
 
-            // If not enough points remaining not on a plane, continue to next trial
-            if (pointCloud.size() - removedPoints.size() < 3) {
-                std::cout << "RANSAC trial " << (trial + 1) << " failed" << std::endl;
-                trial++;
-                continue;
-            }
             // For each trial, generate a plane from 3 random point cloud indexes
             std::vector<size_t> foundPoints;
             while (foundPoints.size() < 3) {
                 size_t index = distr(gen);
-                // Save random point if not already part of a plane
-                if (removedPoints.end() == find(removedPoints.begin(), removedPoints.end(), index))
-                    foundPoints.push_back(index);
+                foundPoints.push_back(pointCloud.remainingPoints[index]);
             }
             Eigen::Hyperplane<double, 3> thisPlane = Eigen::Hyperplane<double, 3>::Through(
-                pointCloud[foundPoints[0]].location,
-                pointCloud[foundPoints[1]].location,
-                pointCloud[foundPoints[2]].location);
+                pointCloud.getPoint(foundPoints[0]).location,
+                pointCloud.getPoint(foundPoints[1]).location,
+                pointCloud.getPoint(foundPoints[2]).location);
             // Add points closer than threshold to this plane
-            std::vector<size_t> thisPoints;
-            //OpenMP requires signed integrals for its loop variables... interesting
-            signed long long i = 0;
-#pragma omp parallel for shared(thisPoints) private (i)
-            for (i = 0; i < pointCloud.size(); ++i) {
-                if (thisPlane.absDistance(pointCloud[i].location) < threshold)
-#pragma omp critical
-                    thisPoints.push_back(i);
-                // limit a single thread on the first trial of the first plane to printing the number of threads being used
-                if (trial == 0 && omp_get_thread_num() == 0 && plane == 0)
-                    threads = omp_get_num_threads();
-            }
+            size_t thisSize = 0;
+            std::vector<bool> thisPoints = pointCloud.planePoints(thisPlane, thisSize);
+
             // Update plane with the most points
-            if (thisPoints.size() > bestPoints.size()) {
+            if (thisSize > bestSize) {
                 bestPlane = thisPlane;
                 bestPoints = thisPoints;
-                inlierRatio = (float)bestPoints.size() / (pointCloud.size() - removedPoints.size());
+                bestSize = thisSize;
+                inlierRatio = (float)bestSize / (pointCloud.remainingPoints.size());
                 numTrials = log(1 - successProb) / log(1 - pow(inlierRatio, 3));
 
             }
@@ -85,19 +65,30 @@ PointCloud ransac(PointCloud pointCloud, std::mt19937 gen, double successProb, d
         // Other operations with bestPlane can be done here
         std::cout << trial << " RANSAC trials run for plane " << plane + 1 << ", equation: " <<
             bestPlane.coeffs()[0] << "x + " << bestPlane.coeffs()[1] << "y + " << bestPlane.coeffs()[2] << "z + " << bestPlane.coeffs()[3] << " = 0" << std::endl;
-        // Save point indexes of the best plane from all trials to be removed
-        for (size_t j = 0; j < bestPoints.size(); ++j) {
-            pointCloud[bestPoints[j]].planeIx = plane;
-            removedPoints.push_back(bestPoints[j]); // opportunity to do reclaiming here?
-            // could only compare planes that intersect close to the bounding box?
-        }
+        // Remove point indexes of the best plane from all trials
+        pointCloud.removePoints(bestPoints, plane);
         plane++;
+        planes.push_back(bestPlane);
 
-    } while ((float)removedPoints.size() / pointCloud.size() < explained);
-    std::cout << omp_get_max_threads() << " threads were used" << std::endl;
+    } while ((float)pointCloud.remainingPoints.size()/pointCloud.size > noise);
 
-    return pointCloud;
+    return planes;
 
+}
+
+
+void recolor(PointCloud pointCloud, std::string outputFile, std::vector<Eigen::Vector3i> colours) {
+    // Recolour points according to their plane then save the results
+    std::cout << "Writing points to " << outputFile << std::endl;
+    signed long long val = 0;
+#pragma omp parallel for num_threads(pointCloud.num_threads)
+    for (val = 0; val < pointCloud.size; ++val) {
+        if (pointCloud.getPoint(val).planeIx >= 0) {
+            pointCloud.setPointColour(val, colours[pointCloud.getPoint(val).planeIx % colours.size()]);
+            // idea: color planes with limited colors based on avoiding intersecting plane's colors
+        }
+    }
+    pointCloud.writeToPly(outputFile);
 }
 
 
@@ -107,74 +98,70 @@ int main(int argc, char* argv[]) {
     std::string inputFile = "";
     std::string outputFile = "";
     float success = 0.99;
-    float explained = 0.99;
+    float noise = 0.01;
     float threshold = -1;
     int maxTrials = 1000;
     float scale_parameter = 0.01;
+    int voxel_size = 0;
 
     // Parse the command line
-    if (argc != 7) {
-        std::cout << "Usage: planeFinder <input file> <output file> <probability of success> <ratio of scene to be explained by planes> <max RANSAC trials> <scale factor>" << std::endl;
+    if (argc < 7 || argc > 8) {
+        std::cout << "Usage: planeFinder <input file> <output file> <probability of success> <fraction of points to leave out> <max RANSAC trials> <scale factor>" << std::endl;
+        std::cout << "optional: \"uniform\" or \"vector\" space subdivision" << std::endl;
         exit(-2);
     }
     else {
         inputFile = argv[1];
         outputFile = argv[2];
         success = atof(argv[3]);
-        explained = atof(argv[4]);
+        noise = atof(argv[4]);
         maxTrials = atoi(argv[5]);
         scale_parameter = atof(argv[6]);
+        if (argc == 8)
+            voxel_size = atoi(argv[7]);
     }
 
     // Set up random seed
     // (random size generation taken from https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution)
     std::random_device rd;
     std::mt19937 gen(rd());
-
-    PointCloud pointCloud = readFromPly(inputFile);
-
-    // Checking if number of points is too big for signed long long type (this aint gonna happen lmao)
-    if (pointCloud.size() > LLONG_MAX) {
-        std::cout << "Model is too big - reduce points to " << LLONG_MAX << " or less" << std::endl;
-        return 1;
-    }
-
-    // Determine the threshold as a % of model size
-    // (coordinate center is all over the place, so biggest/smallest signed point difference gives bounding box)
-    double xs = pointCloud[0].location[0];
-    double xl = pointCloud[0].location[0];
-    double ys = pointCloud[0].location[1];
-    double yl = pointCloud[0].location[1];
-    double zs = pointCloud[0].location[2];
-    double zl = pointCloud[0].location[2];
-    // chunk parallelize this if its slow?
-    for (size_t i = 1; i < pointCloud.size(); i++) {
-        xs = std::min(xs, pointCloud[i].location[0]);
-        xl = std::max(xl, pointCloud[i].location[0]);
-        ys = std::min(ys, pointCloud[i].location[1]);
-        yl = std::max(yl, pointCloud[i].location[1]);
-        zs = std::min(zs, pointCloud[i].location[2]);
-        zl = std::max(zl, pointCloud[i].location[2]);
-    }
-    // get x/y/z difference and compute average scale factor for the model
-    double scale = (xl - xs + yl - ys + zl - zs) / 3;
-    // apply a small % to the value to get a sensible threshold
-    threshold = scale_parameter * scale;
-    std::cout << "Auto-generated threshold is " << threshold << std::endl;
-
     // Set up some colours to assign to the planes that are found
     std::vector<Eigen::Vector3i> colours;
     initialiseColours(&colours);
 
-    pointCloud = ransac(pointCloud, gen, success, explained, threshold, maxTrials);
+    std::vector<Eigen::Hyperplane<double, 3>> planes;
 
-    // Recolour points according to their plane then save the results
-    std::cout << "Writing points to " << outputFile << std::endl;
-    for (auto& point : pointCloud) {
-        if (point.planeIx >= 0) {
-            point.colour = colours[point.planeIx % colours.size()];
-            // idea: color planes with limited colors based on avoiding intersecting plane's colors
-        }
+    //SET NUM THREADS HERE
+    unsigned int threads = 1;
+
+    PointCloud pointCloud = PointCloud(inputFile, scale_parameter, threads);
+    // Checking if number of points is too big for signed long long type
+    if (pointCloud.size > LLONG_MAX) {
+        std::cout << "Model is too big - reduce points to " << LLONG_MAX << " or less" << std::endl;
+        return 1;
     }
-    writeToPly(pointCloud, outputFile);
+    threshold = pointCloud.threshold;
+    std::cout << "Auto-generated threshold is " << threshold << std::endl;
+    if (planes.size() > colours.size()) std::cout << "Warning: more planes than colours" << std::endl;
+
+    if (argc == 8) {
+        UniformPC u = UniformPC(pointCloud, voxel_size);
+        // 3 runs for ransac average time
+        //ransac(u, gen, success, noise, threshold, maxTrials);
+        //u.resetRemaining();
+        //ransac(u, gen, success, noise, threshold, maxTrials);
+        //u.resetRemaining();
+        std::vector<Eigen::Hyperplane<double, 3>> planes = ransac(u, gen, success, noise, threshold, maxTrials);
+        std::cout << "Total point distance calculations made: " << u.comparisons << std::endl;
+        recolor(u, outputFile, colours);
+    }
+    else {
+        //ransac(pointCloud, gen, success, noise, threshold, maxTrials);
+        //pointCloud.resetRemaining();
+        //ransac(pointCloud, gen, success, noise, threshold, maxTrials);
+        //pointCloud.resetRemaining();
+        std::vector<Eigen::Hyperplane<double, 3>> planes = ransac(pointCloud, gen, success, noise, threshold, maxTrials);
+        std::cout << "Total point distance calculations made: " << pointCloud.comparisons << std::endl;
+        recolor(pointCloud, outputFile, colours);
+    }
 }
