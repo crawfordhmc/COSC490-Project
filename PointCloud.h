defe9ba8a657b7c2df5cb0e5d6c88921806bcc1f
crawfordhmc <43801183+crawfordhmc@users.noreply.github.com>
2020-10-26 19:13:35 +1300

    Update PointCloud.h

--- PointCloud.h
+++ PointCloud.h
@@ -11,16 +11,45 @@
 #include <random>
 #include <omp.h>
 
-struct Point {
-	Eigen::Vector3d location;
-	Eigen::Vector3i colour;
-	int planeIx;
+class PointCloud {
+
+public:
+	struct Point {
+		Eigen::Vector3d location;
+		Eigen::Vector3i colour;
+		int planeIx;
+	};
+
+	size_t size = 0;
+	double threshold;
+	std::vector<size_t> remainingPoints;
+	size_t comparisons = 0;
+	unsigned int num_threads = 1;
+
+	PointCloud(const std::string& filepath, float scale_parameter, unsigned int threads);
+
+	virtual std::vector<bool> planePoints(const Eigen::Hyperplane<double, 3> &thisPlane, size_t &thisSize);
+
+	Point getPoint(size_t index);
+	void setPointPlane(size_t index, int planeID);
+	void setPointColour(size_t index, const Eigen::Vector3i &colour);
+	void writeToPly(const std::string& filename);
+	virtual void removePoints(const std::vector<bool>& planePoints, int plane);
+	virtual void resetRemaining();
+
+protected:
+	std::vector<Eigen::Vector3f> verts;
+	std::vector<Eigen::Matrix<uint8_t, 3, 1>> cols;
+
+	std::vector<Point> pc;
+
+	// bounding box corners
+	double XS;
+	double XL;
+	double YS;
+	double YL;
+	double ZS;
+	double ZL;
 };
-
-typedef std::vector<Point> PointCloud;
-
-PointCloud readFromPly(const std::string& filename);
-
-void writeToPly(const PointCloud& pc, const std::string& filename);
 
 #endif
